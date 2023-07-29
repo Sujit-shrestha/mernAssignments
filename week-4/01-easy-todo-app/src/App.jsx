0e15589aed67e7b89fd3af0747977db4573c3ff2
sujit <sujitshrestha0004@gmail.com>
2023-07-29 21:38:33 +0545

    Fetches from backed and displays dynamically uising react components

--- week-4/01-easy-todo-app/src/App.jsx
+++ week-4/01-easy-todo-app/src/App.jsx
@@ -2,16 +2,56 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
-function App() {
+function useTodos(){
   const [todos, setTodos] = useState([])
     // fetch all todos from server
+    useEffect(()=>{
+      fetch("http://localhost:3000/todos" , {
+      method : "GET"
+    }).then((response)=> {
+      response.json().then((data)=>{
+        console.log(data);
+        setTodos(data);
+      })
+    })
+
+    setInterval(() => {
+      fetch("http://localhost:3000/todos" , {
+      method : "GET"
+    }).then((response)=> {
+      response.json().then((data)=>{
+        console.log(data);
+        setTodos(data);
+      })
+    })
+    }, 15000);
+    } , [])
+    
+
+  return todos;
+}
+
+function App() {
+ const todos = useTodos();
 
   return (
     <>
       <div>
         <h1>Easy Todo App</h1>
-        <input type="text" />
+        {todos.map((todo)=>{
+          return (
+            <div class="todo-container">
+              <div class="todo-title">{todo.title}</div>
+              <div class="todo-description">{todo.description}</div>
+              {todo.completed}
+              <Todo></Todo>
+            </div>
+            
+          )
+        })}
+      
       </div>
     </>
   )
@@ -20,7 +60,7 @@ function App() {
 function Todo(props) {
     // Add a delete button here so user can delete a TODO.
     return <div>
-        {props.title}
+      <button>Delete</button> 
     </div>
 }
 
