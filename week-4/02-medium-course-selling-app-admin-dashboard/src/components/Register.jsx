927f406b2128c18ace57049ed324fea3c4665a12
sujit <sujitshrestha0004@gmail.com>
2023-07-30 16:41:27 +0545

    Registration/Signup occurs correrctly

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-/// File is incomplete. You need to add input boxes to take input for users to register.
+function registration(email, password) {
+  
+    const dataForBody = {
+      username: email,
+      password: password,
+    };
+    fetch("http://localhost:3000/admin/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json", // Specify the content type as JSON
+      },
+      body: JSON.stringify(dataForBody),
+    })
+      .then((response) => response.json())
+      .then((jsonifiedData) => {
+        console.log("The registered data is: ", jsonifiedData);
+      });
+}
+
+
 function Register() {
-    const [email, setEmail] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    return <div>
-        <h1>Register to the website</h1>
-        <br/>
-        <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        Already a user? <a href="/login">Login</a>
+  const handleRegister = () => {
+    registration(email, password);
+  };
+
+  return (
+    <div>
+      <h1>Register to the website</h1>
+      <br />
+      <label>Email:</label>
+      <input type="text" onChange={(e) => setEmail(e.target.value)} />
+      <br />
+      <label>Password:</label>
+      <input type="password" onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <button onClick={handleRegister}>Register</button>
+      Already a user? <a href="/login">Login</a>
     </div>
+  );
 }
 
 export default Register;
