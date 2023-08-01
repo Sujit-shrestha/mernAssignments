67f22d180f1ed0b268194a658bc7129e251de6c9
sujit <sujitshrestha0004@gmail.com>
2023-08-01 22:47:05 +0545

    Added code to fetch the courses from endpoint

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/ShowCourses.jsx
@@ -1,11 +1,43 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import Landing from "./Landing";
+import Cookies from "js-cookie";
 function ShowCourses() {
     const [courses, setCourses] = React.useState([]);
+    let currentToken = Cookies.get("token");
+    
+   // Add code to fetch courses from the server
+    useEffect(
+        ()=>{
+            fetch("http://localhost:3000/admin/courses",{
+                method:"GET",
+                headers:{"Authorization" : currentToken}
+                }).then((response)=>{
+                         return response.json()
+                }).then(
+                     (data)=>{
+                    setCourses(data.courses)
+                })
 
-    // Add code to fetch courses from the server
+            setInterval(() => {
+                fetch("http://localhost:3000/admin/courses",{
+                method:"GET",
+                headers:{"Authorization" : currentToken}
+                }).then((response)=>{
+                         return response.json()
+                }).then(
+                     (data)=>{
+                    setCourses(data.courses)
+                })
+            }, 15000);
+            
+        } , [])
+   
+
+    
+    
     // and set it in the courses state variable.
     return <div>
+        <Landing/>
         <h1>Create Course Page</h1>
         {courses.map(c => <Course title={c.title} />)}
     </div>
@@ -13,6 +45,7 @@ function ShowCourses() {
 
 function Course(props) {
     return <div>
+        
         <h1>{props.title}</h1>
     </div>
 }
